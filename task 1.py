cba25548dab6fe86a2d2bf989698d236aecdfc38
jai20-d <jaitomarjai20@gmail.com>
2025-08-24 19:43:50 +0530

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,16 @@
+a=input('enter the first number: ')
+b=input('enter the second number: ')
+a=float(a)
+b=float(b)
+
+addition=a+b
+subtraction=a-b
+multiplication=a*b
+division=a/b
+
+print('addition =',addition)
+print('subtraction =',subtraction)
+print('multiplication =',multiplication)
+print('division =',division)
+
+
---
+++ task 2.py
@@ -0,0 +1,6 @@
+a=input('enter your first name: ')
+b=input('enter your second name: ')
+c=a +" "+b
+print(f'hello,  {c}! Welcome to the python program.')
+
+
